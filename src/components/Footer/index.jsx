3c70896d283c27159c2f99b89c0b12103f1d2338
Mariegolepas <mariegolepastwitch@gmail.com>
2023-01-26 16:41:44 +0100

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import '.../styles/Footer.cs'
+
+function Footer() {
+	return (
+		<footer className='footer'>
+            <img src='kasa\src\assets\logo.svg' alt='Logo Kasa' className='footer__logo'/>
+			<div className='footer__div'>
+                © 2020 Kasa. All rights reserved
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Routes } from 'react-router-dom' //Used to be 'Switch' in past Router version
 //import Home from './pages/Home'
 import App from './components/App'
-import Header from './components/Header'
-import Error from './components/Error'
+import Header from './components/Header/index'
+import Error from './components/Error/index'
+import Footer from './components/Footer/index'
  
 ReactDOM.render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.render(
                   <Error />
               </Route>
           </Routes>
+          <Footer />
       </Router>
   </React.StrictMode>,
 document.getElementById('root')
