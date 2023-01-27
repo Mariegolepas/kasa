6c86c3f3e1f3d8ce561baa4c008f57c1f3c04ff3
Mariegolepas <mariegolepastwitch@gmail.com>
2023-01-27 16:31:54 +0100

    first fix on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
-import { logements } from '../datas/logements.json'
+import React from 'react'
+import logements from '../datas/logements.json'
 import Card from '../components/Card/index'
+import Banner from '../components/Banner/index'
+import Carrousel from '../components/Carrousel'
+import '../styles/Home.css'
 
 function Logements() {
     return (
-        <div>
-            <h1>Page d'accueil</h1>
+        <div className='home'>
+            <Banner>
+                <div className='home__banner'>
+                    <img src='../assets/mountain.png' alt='Falaises' className='home__banner__image' />
+                    <h1 className='home__banner__title'>
+                        Chez vous, partout et ailleurs
+                    </h1>
+                </div>
+            </Banner>
+            <Carrousel>
+                {logements.map((Logements) => (
+                    <Card
+                        key={Logements.id}
+                        id={Logements.id}
+                        picture={Logements.cover}
+                        title={Logements.title}
+                    />))}
+            </Carrousel>
+        </div>
+    )
+}
+
+export default Logements
+
+/* {<h1>Page d'accueil</h1>
             {logements.map((logement, index) => (
                 <Card
                     key={`${logement.name}-${index}`}
@@ -12,9 +39,4 @@ function Logements() {
                     picture={logement.picture}
                     title={logement.name}
                 />
-            ))}
-        </div>
-    )
-}
-
-export default Logements
+            ))}} */
