44f8261d1140fc02b42bcc68253ffcfec6a2c210
Mariegolepas <mariegolepastwitch@gmail.com>
2023-01-27 16:30:42 +0100

    first fix Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom'
-
+import { NavLink } from 'react-router-dom'
+import '../../styles/Header.css'
+import logo from '../../assets/logo.svg'
  
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey">Questionnaire</Link>
-        </nav>
+        <header className='header'>
+            {/* <nav className='header__navbar'> */}
+            <img src={logo} alt='Logo Kasa' className='header__logo'/>
+            <nav className='header__navbar'>
+                <ul className='header__navbar__list'>
+                    <li className='header__navbar__list__bloc'><NavLink to="/" activeClassName={({isActive}) => isActive ? 'header__navbar__list__bloc--active' : undefined} end>Accueil</NavLink></li>
+                    <li className='header__navbar__list__bloc'><NavLink to="/about" activeClassName={({isActive}) => isActive ? 'header__navbar__list__bloc--active' : undefined}>A Propos</NavLink></li>
+                </ul> 
+            </nav>
+        </header>
     )
 }
 
