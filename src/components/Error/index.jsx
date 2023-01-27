db48e6673e0fff0c483bbcbbbb91e68d8ff4c941
Mariegolepas <mariegolepastwitch@gmail.com>
2023-01-27 16:29:41 +0100

    first fix Error

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+import '../../styles/Error.css'
+
 function Error() {
     return (
-        <div>
-            <h1 className="Error404">404</h1>
-            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <div className='error'>
+            <h1 className='error__title'>404</h1>
+            <h2 className='error__description'>Oups! La page que vous demandez n'existe pas.</h2>
+
+            <Link to="/" className='error__link'>Retourner sur la page d'accueil</Link>
         </div>
     )
 }
