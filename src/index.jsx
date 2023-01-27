11e7ed827096d006e54b205d06d21cbcceee4d9f
Mariegolepas <mariegolepastwitch@gmail.com>
2023-01-27 16:34:56 +0100

    fix index doc

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Routes } from 'react-router-dom' //Used to be 'Switch' in past Router version
-//import Home from './pages/Home'
+import { BrowserRouter} from 'react-router-dom'
 import App from './components/App'
-import Header from './components/Header/index'
-import Error from './components/Error/index'
-import Footer from './components/Footer/index'
+
  
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>,
+document.getElementById('root')
+)
+
+/* import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+//import { Routes } from 'react-router-dom' //Used to be 'Switch' in past Router version
+//import Home from './pages/Home'
+import App from './components/App'
+import Header from './components/Header'
+import Error from './components/Error'
+ 
+ReactDOM.render(
       <Router>
           <Header />
           <Routes>
@@ -20,8 +31,5 @@ ReactDOM.render(
                   <Error />
               </Route>
           </Routes>
-          <Footer />
-      </Router>
-  </React.StrictMode>,
-document.getElementById('root')
-)
+      </Router>,
+document.getElementById('root') */
