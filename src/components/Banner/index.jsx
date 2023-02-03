cf2dfe36940b56700e30670bef1a977b2f3bddf5
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-03 14:45:51 +0100

    edit Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,29 @@
 import '../../styles/Banner.css'
+import PropTypes from 'prop-types'
 
-function Banner({children}) {
+Banner.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    alt: PropTypes.string,
+}
+
+function Banner({image, title, alt}) {
+    return (
+        <div className='banner'>
+            <img src={image} alt={alt} className='banner__image' />
+            <h1 className='banner__title'>
+                {title}
+            </h1>
+        </div>
+    );
+}
+
+/* function Banner({children}) {
     return (
         <div className='banner'>
             {children}
         </div>
     );
-}
+} */
 
 export default Banner;
