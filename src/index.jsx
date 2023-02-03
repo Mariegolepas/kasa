84d95325a4f6af3d1518209220cfd47bd4124e81
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-03 14:51:17 +0100

    fix react router use

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom'
 import App from './components/App'
 
- 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+      <App/>
+  </BrowserRouter>
+);
+
+/* ReactDOM.render(
   <BrowserRouter>
       <App/>
   </BrowserRouter>,
 document.getElementById('root')
-)
-
-/* import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-//import { Routes } from 'react-router-dom' //Used to be 'Switch' in past Router version
-//import Home from './pages/Home'
-import App from './components/App'
-import Header from './components/Header'
-import Error from './components/Error'
- 
-ReactDOM.render(
-      <Router>
-          <Header />
-          <Routes>
-              <Route exact path="/">
-                  <App />
-              </Route>
-              <Route>
-                  <Error />
-              </Route>
-          </Routes>
-      </Router>,
-document.getElementById('root') */
+) */
