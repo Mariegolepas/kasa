930f2698e1b1cb03bcc86071425d1c8816c57121
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-03 14:50:05 +0100

    fix & add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,24 @@ import Error from './Error/index'
 import Footer from './Footer/index'
 import Home from '../pages/Home'
 import Logement from '../pages/Logement'
+import About from '../pages/About'
 import '../styles/index.css'
 import { Routes, Route } from 'react-router-dom' //Used to be 'Switch' in past Router version
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path={'/logement/:id'} element={<Logement />} />
-          <Route path='*' element={<Error/>} />
-      </Routes>
-      <Footer />
+      <div className='container'>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path={'/logement/:id'} element={<Logement />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/error' element={<Error/>} />
+            <Route path='*' element={<Error/>} />
+        </Routes>
+      </div>
+    <Footer />
     </>
   );
 }
