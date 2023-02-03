78607dbf0caf73b166e74d7946d4dd2807c8dce2
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-04 00:25:48 +0100

    add Logement informations

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
+import Tag from '../components/Tag'
+import Collapse from '../components/Collapse'
+import Slide from '../components/Slide'
+import '../styles/Logement.css'
+import { useParams, /* useNavigate */ } from 'react-router-dom'
+import logements from '../datas/logements.json'
+import Error from '../components/Error'
 
 function Logement() {
-    return (
-        <div>
-            TEST
-        </div>
-    );
+    const params = useParams();
+    const id = params.id;
+    const logement = logements.find((e) => e.id === id);
+    const tags = logement.tags;
+
+    //const navigate = useNavigate()
+
+    console.log(logement);
+
+    if (logement !== undefined) {
+        return (
+            <div className='logement'>
+                <Slide
+                    pictures={logement.pictures}
+                />
+                <div className='logement__header'>
+                    <div className='logement__leftpart'>
+                        <h1 className='logement__title'>{logement.title}</h1>
+                        <h2 className='logement__location'>{logement.location}</h2>
+
+                        <div className='tag'>
+                            {tags.map(tag =>
+                                <Tag 
+                                    key={tag}
+                                    tag={tag}
+                                />)
+                            }
+                        </div>
+                    </div>
+                    <div className='logement__rightpart'>
+                        <div className='logement__people'>
+                            <div className='logement__people__name'>{logement.host.name}</div>
+                            <img src={logement.host.picture} alt="avatar de l'host" className='logement__people__pic' />
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='collapses'>
+                    <Collapse
+                        title={'Description'}
+                        size={'45'}
+                        infos={logement.description}
+                    />
+                    <Collapse
+                        title={'Équipement'}
+                        size={'45'}
+                        infos={logement.equipments}
+                    />
+                </div>
+                
+                
+            </div>
+        );
+    } else {
+        return <Error />;
+    }
+
+    
 }
 
-export default Logement;
+export default Logement
