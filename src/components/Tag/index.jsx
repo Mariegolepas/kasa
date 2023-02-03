130675d72588b6ac396a83fd632784eeb9acb48c
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-04 00:25:08 +0100

    edit tag

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -2,11 +2,9 @@ import '../../styles/Tag.css'
 
 function Tag({tag}) {
     return (
-        <div>
             <div className="tag__div">
                 <p className="tag__text">{tag}</p>
             </div>
-        </div>
     )
 }
  
