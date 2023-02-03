dd5dc2eb034e9e00a8c525c89d7095151effdbcf
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-03 14:49:08 +0100

    add about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Collapse from '../components/Collapse'
+import Banner from '../components/Banner'
+import '../styles/Logement.css'
+import Lake from '../assets/lake-mountains.png'
+
+function About() {
+    return (
+        <div className='about'>
+            <Banner 
+                image={Lake}
+                title={''}
+                alt={'Lac avec des montagnes autour'}
+            />
+
+            <Collapse
+                title={'Fiabilité'}
+                size={'90'}
+                infos={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."}
+            />
+
+            <Collapse
+                title={'Respect'}
+                size={'90'}
+                infos={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}
+            />
+
+            <Collapse
+                title={'Services'}
+                size={'90'}
+                infos={"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."}
+            />
+
+            <Collapse
+                title={'Sécurité'}
+                size={'90'}
+                infos={"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."}
+            />
+        </div>
+    );
+}
+
+export default About
