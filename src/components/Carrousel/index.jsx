1946a0c48676ff003ac20f07d8d8819f6d99e8b7
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-06 00:38:31 +0100

    add carrousel comments

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -1,5 +1,12 @@
+//Import our style for this page
 import '../../styles/Carrousel.css'
 
+/**
+ * Container Component which will be have our cards has children on homepage.
+ * Component which can be reuse later
+ * @param {*} children because can have many things inside 
+ * @returns our component
+ */
 function Carrousel({children}) {
     return (
         <main className='carrousel'>
