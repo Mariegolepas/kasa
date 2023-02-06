ecca21913b3143b3c429d2a2fa43c9a45c770bc5
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-06 14:38:10 +0100

    add logo redirection + some comments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,18 @@
+//Import NavLink from React Router v6 - permit to recognize while our page is active
 import { NavLink } from 'react-router-dom'
+//Import our style for this page
 import '../../styles/Header.css'
+//Import our logo
 import logo from '../../assets/logo.svg'
  
+/**
+ * Our Header component, which include our Navbar
+ * @returns our component
+ */
 function Header() {
     return (
         <header className='header'>
-            {/* <nav className='header__navbar'> */}
-            <img src={logo} alt='Logo Kasa' className='header__logo'/>
+            <NavLink to="/"><img src={logo} alt='Logo Kasa' className='header__logo'/></NavLink>
             <nav className='header__navbar'>
                 <ul className='header__navbar__list'>
                     <li className='header__navbar__list__bloc'><NavLink to="/" className={({isActive}) => isActive ? 'header__navbar__list__bloc--active' : undefined} end>Accueil</NavLink></li>
