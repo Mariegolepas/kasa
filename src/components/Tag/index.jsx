480666d81d43cdaaf80b5508759ad127248e0e63
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-06 14:48:36 +0100

    add Tag comments

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,5 +1,11 @@
+//Import our style for this page
 import '../../styles/Tag.css'
 
+/**
+ * Our Tag components
+ * @param {*} tag our tag information to display
+ * @returns our component
+ */
 function Tag({tag}) {
     return (
             <div className="tag__div">
