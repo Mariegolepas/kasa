76a782c9d85f70a79850d241b6390560f08d63bf
Mariegolepas <mariegolepastwitch@gmail.com>
2023-02-06 14:56:38 +0100

    add some comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
+//Import our general components
 import Header from './Header/index'
 import Error from './Error/index'
 import Footer from './Footer/index'
+//Import our pages
 import Home from '../pages/Home'
 import Logement from '../pages/Logement'
 import About from '../pages/About'
+//Import our style for this page
 import '../styles/index.css'
+//Import React Router v6 Components for our routes
 import { Routes, Route } from 'react-router-dom' //Used to be 'Switch' in past Router version
 
+/**
+ * Our routes for our website
+ * @returns 
+ */
 function App() {
   return (
     <>
